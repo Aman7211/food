7137b7e9aac0aa83ae3393741df883aaff26b0c8
Aman7211 <amanrathore9811@gmail.com>
2023-06-02 16:28:46 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ app.use((req,res,next)=>{
 app.get('/', (req, res) => {
   res.send('Hello World!  Hi i am Aman Kumar ')
 })
+
 app.use(express.json())
 app.use('/api', require("./Routes/CreateUser"));
 app.use('/api', require("./Routes/DisplayData"));
